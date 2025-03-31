8178d79414259c6c9206c2b3890bb5783f5ceb7e
simoncanorio <sicanoriohellwing@mail.austral.edu.ar>
2025-03-31 11:55:05 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -3,6 +3,6 @@ def earth():
     y = "Barbados"
     resultado_x_y = x[3] > y[3]
     resultado_y_x = x[3] < y[3]
-    print(f"""The result of X comes first in the dictionary than Y is {resultado_x_y}.
-The result of Y comes first in the dictionary than X is {resultado_y_x}""")
+    print(f"""The result of {x} comes first in the dictionary than {y} is {resultado_x_y}.
+The result of {y} comes first in the dictionary than {x} is {resultado_y_x}""")
 earth()
